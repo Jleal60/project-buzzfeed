ebb96ad0a0c31af6a9290fd815fe0a2749f985c6
Jleal60 <134532990+Jleal60@users.noreply.github.com>
2024-04-02 14:01:18 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,145 @@
+let optionOne = document.querySelector(".option-one-screen");
+console.log(optionOne);
+let optionOneButton = document.querySelector(".option-one");
+
+optionOneButton.addEventListener("click", function() {
+  optionOne.style.display = "block";
+});
+console.log(optionOneButton);
+
+
+
+let optionTwo = document.querySelector(".option-two-screen");
+console.log(optionTwo);
+let optionTwoButton = document.querySelector(".option-two");
+
+optionTwoButton.addEventListener("click", function() {
+  optionOne.style.display = "block";
+});
+console.log(optionTwoButton);
+
+
+
+let optionThree = document.querySelector(".option-three-screen");
+console.log(optionThree);
+let optionThreeButton = document.querySelector(".option-three");
+
+optionThreeButton.addEventListener("click", function() {
+  optionOne.style.display = "block";
+});
+console.log(optionThreeButton);
+
+let optionFour = document.querySelector(".option-four-screen");
+console.log(optionFour);
+let optionFourButton = document.querySelector(".option-four");
+
+optionFourButton.addEventListener("click", function() {
+  optionOne.style.display = "block";
+});
+console.log(optionFourButton);
+
+let image = document.querySelector(".bruh");
+console.log(image);
+
+image.addEventListener("click", function() {
+  optionFive.style.display = "block";
+});
+
+
+
+
+let optionFive = document.querySelector(".option-five-screen");
+console.log(optionFive);
+let optionFiveButton = document.querySelector(".option-five");
+
+optionFiveButton.addEventListener("click", function() {
+  optionSix.style.display = "block";
+});
+console.log(optionFiveButton);
+
+
+
+let optionSix = document.querySelector(".option-six-screen");
+console.log(optionSix);
+let optionSixButton = document.querySelector(".option-six");
+
+optionSixButton.addEventListener("click", function() {
+  optionSix.style.display = "block";
+});
+console.log(optionSixButton);
+
+
+
+let optionSeven = document.querySelector(".option-seven-screen");
+console.log(optionSeven);
+let optionSevenButton = document.querySelector(".option-seven");
+
+optionSevenButton.addEventListener("click", function() {
+  optionEight.style.display = "block";
+});
+console.log(optionSevenButton);
+
+let optionEight = document.querySelector(".option-eight-screen");
+console.log(optionEight);
+let optionEightButton = document.querySelector(".option-eight");
+
+optionEightButton.addEventListener("click", function() {
+  optionEight.style.display = "block";
+});
+console.log(optionEightButton);
+
+
+
+let optionNine = document.querySelector(".option-nine-screen");
+console.log(optionNine);
+let optionNineButton = document.querySelector(".option-nine");
+
+optionNineButton.addEventListener("click", function() {
+  optionEight.style.display = "block";
+});
+console.log(optionNineButton);
+
+
+
+
+let optionTen = document.querySelector(".option-ten-screen");
+console.log(optionTen);
+let optionTenButton = document.querySelector(".option-ten");
+
+optionTenButton.addEventListener("click", function() {
+  optionEight.style.display = "block";
+});
+console.log(optionTenButton);
+
+
+
+let optionEleven = document.querySelector(".option-elven-screen");
+console.log(optionEleven);
+let optionElevenButton = document.querySelector(".option-eleven");
+
+optionElevenButton.addEventListener("click", function() {
+  optionEleven.style.display = "block";
+});
+console.log(optionElevenButton);
+
+
+
+let optionTwelve = document.querySelector(".option-twelve-screen");
+console.log(optionTwelve);
+let optionTwelveButton = document.querySelector(".option-twelve");
+
+optionTwelveButton.addEventListener("click", function() {
+  optionTwelve.style.display = "block";
+});
+console.log(optionTwelveButton);
+
+
+
+let optionThirteen = document.querySelector(".option-thirteen-screen");
+console.log(optionThirteen);
+let optionThirteenButton = document.querySelector(".option-thirteen");
+
+optionThirteenButton.addEventListener("click", function() {
+  optionThirteen.style.display = "block";
+});
+console.log(optionTwelveButton);
